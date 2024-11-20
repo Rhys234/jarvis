e6049acd6d22df354843c4dde2f01c731d0bfd0f
Rhys <C.Sutter.inf22@stud.bbbaden.ch>
2024-11-20 10:49:18 +0100

    daten analyse eingefügt

--- JarvisMain.py
+++ JarvisMain.py
@@ -1,5 +1,6 @@
 import speech_recognition as sr
 import pyttsx3
+import requests
 
 # Initialisiere Spracherkennung und Sprachausgabe
 recognizer = sr.Recognizer()
@@ -25,3 +26,16 @@ def listen():
         except sr.RequestError:
             speak("Network error. Please check your connection.")
             return None
+
+def get_weather(location="Berlin"):
+    """Holt Wetterdaten für den angegebenen Ort."""
+    API_KEY = "deine_openweathermap_api_key"  # Registriere dich bei openweathermap.org
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={location}&appid={API_KEY}&units=metric"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        weather = data["weather"][0]["description"]
+        temp = data["main"]["temp"]
+        return f"The weather in {location} is {weather} with a temperature of {temp}°C."
+    else:
+        return "I couldn't retrieve the weather data."
